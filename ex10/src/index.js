b8bfaea667d6231ed5447586a14f88dc14c01709
Vedran V <drvicko@hotmail.com>
2022-04-04 21:48:01 +0200

    ex10 the parseInt(); Function

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+// Write code below this line
+
+function convertStrToInt (str) {
+    return parseInt(str);
+}
+
+convertStrToInt();
+
+// Write code above this line
+
+
+module.exports = convertStrToInt;
