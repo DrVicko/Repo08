797f850c7db2b9525d7b3f4e18d1bc2f804d186b
Vedran V <drvicko@hotmail.com>
2022-04-04 20:35:36 +0200

    ex02 Functions with Parameters

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+// Write code below this line
+function functionPrintInfo(name, age) {
+    console.log(name, age);
+}
+console.log(functionPrintInfo("John", 23)); 
+// Write code above this line
+
+module.exports = reusableFunction; 
