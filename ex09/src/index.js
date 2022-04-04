e4677ac063607d38c371c8585d4906318c2f4f70
Vedran V <drvicko@hotmail.com>
2022-04-04 21:40:00 +0200

    ex09 Generate Random Whole Numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+// Write code below this line
+
+function randomInteger () {
+    return Math.floor(Math.random() * 30);
+}
+
+randomInteger();
+
+// Write code above this line
+
+
+module.exports = randomInteger;
