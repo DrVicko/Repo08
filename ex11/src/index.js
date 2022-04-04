bf8079a99cbf7a67df5a369d9be1887a56c8aa4a
Vedran V <drvicko@hotmail.com>
2022-04-04 22:05:53 +0200

    ex12 Parse function with radix

---
+++ ex11/src/index.js
@@ -0,0 +1,13 @@
+// Write code below this line
+
+function convertToInt (str, baza) {
+ var x = parseInt(str, baza);
+ return x;
+}
+
+convertToInt("F1", 16);
+
+// Write code above this line
+
+
+module.exports = convertToInt;
