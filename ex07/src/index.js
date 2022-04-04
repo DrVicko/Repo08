9b1315325477376811bcfc2f3b0bf8d658926fa5
Vedran V <drvicko@hotmail.com>
2022-04-04 21:30:00 +0200

    ex07 Global vs Local Scope

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+// Write code below this line
+
+var dress = "little dress"; 
+
+function myDress() {
+    var dress = "ball gown";
+    return dress;
+}
+
+myDress(); 
+
+// Write code above this line
+
+
+module.exports = myDress;
